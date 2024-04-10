4335bfe39de893ef4a5969694d5b2f37f95b5995
jaranchai <jaranchai.nt@hotmail.com>
2024-04-10 15:42:20 +0700

    [Updated] spy self in same file

---
+++ __test__/unit/06-spy/02-spy-self.test.js
@@ -0,0 +1,19 @@
+const calculate = require('../../../src/function/mock/01-calculate');
+
+describe('Spy self', () => {
+  afterEach(() => {
+    // restore the spy created with spyOn
+    jest.restoreAllMocks();
+  });
+
+  it('Should be called add when call doubleSum', () => {
+    const x = 2;
+    const y = 2;
+
+    const calculateAddSpy = jest.spyOn(calculate, 'add');
+
+    calculate.doubleSum(x, y);
+
+    expect(calculateAddSpy).toHaveBeenCalledTimes(1);
+  });
+});
--- src/function/mock/01-calculate.js
+++ src/function/mock/01-calculate.js
@@ -1,8 +1,17 @@
-const math = require('../math');
+// const math = require('../math');
 
 function doubleSum(x, y) {
-  const sum = math.add(x, y);
+  // use add in math.js
+  // const sum = math.add(x, y);
+
+  // use add in 01-calculate.js
+  // const sum = this.add(x, y);
   return x + y + sum;
 }
 
-module.exports = { doubleSum };
+// use spy self test
+// function add(x, y) {
+//   return x + y;
+// }
+
+module.exports = { doubleSum, add };
